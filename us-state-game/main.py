e47ae553c6fa89ed1dd25f0dff9a34b14c7dead1
Amogh Dikshit <amoghdikshit24@gmail.com>
2025-06-11 23:58:42 -0700

    us-state-game

--- us-state-game/main.py
+++ us-state-game/main.py
@@ -31,10 +31,7 @@ while len(guessed_states) < 50:
         t.write(answer)
 
     if answer == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("us-state-game/states_to_learn.csv")
         break
